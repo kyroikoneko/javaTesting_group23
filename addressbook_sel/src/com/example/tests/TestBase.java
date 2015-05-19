68400a0233337cb20bb5754bede8f8ec2c2f7992
kyroikoneko <bloyh@mail.ru>
2015-05-19 16:58:21 +0300

    4.11
    доделать дома, если будет интернет.

--- addressbook_sel/src/com/example/tests/TestBase.java
+++ addressbook_sel/src/com/example/tests/TestBase.java
@@ -3,83 +3,78 @@ package com.example.tests;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
-
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
-
+import static com.example.tests.GroupDataGenerator.generateRandomGroups;
 import com.example.fw.ApplicationManager;
 
 public class TestBase {
-	
+
 	protected static ApplicationManager app;
 
-	
 	@BeforeTest
 	public void setUp() throws Exception {
 		app = new ApplicationManager();
 
-	  }
-	
+	}
+
 	@AfterTest
 	public void tearDown() throws Exception {
 		app.stop();
 
-	  }
+	}
 
-	
 	@DataProvider
-	public Iterator<Object[]> randomValidGroupGenerator(){
+	public Iterator<Object[]> randomValidGroupGenerator() {
+		return wrapGroupsForDataProvider(generateRandomGroups(5)).iterator();
+	}
+
+	private List<Object[]> wrapGroupsForDataProvider(List<GroupData> groups) {
 		List<Object[]> list = new ArrayList<Object[]>();
-		for(int i =0;i<3;i++){
-			GroupData group = new GroupData()
-			.withName(generateRandomString(10))
-			.withFooter(generateRandomString(100))
-			.withHeader(generateRandomString(100));
-		
-			list.add(new Object[]{group});
+		for (GroupData group : groups) {
+			list.add(new Object[] { group });
+
 		}
+		return list;
+	}
+
+	@DataProvider
+	public Iterator<Object[]> randomValidContactGenerator() {
+		List<Object[]> list = new ArrayList<Object[]>();
+		// for(int i =0;i<1;i++){
+		// ContactData contact = new ContactData()
+		// .withName(generateRandomString(10))
+		// .withSecondName(generateRandomString(10))
+		// .withPostAddress(generateRandomString(20))
+		// .withHomePhoneNum(app.getContactHelper().phoneRandomizer())
+		// .withMobilePhonNum(app.getContactHelper().phoneRandomizer())
+		// .withWorkPhoneNum(app.getContactHelper().phoneRandomizer())
+		// .withPostPrimary(app.getContactHelper().emailRandomizer())
+		// .withPostSecondary(app.getContactHelper().emailRandomizer())
+		// .withDay("18")
+		// .withMonth("January")
+		// .withYear("1992")
+		// .withSecondaryPostAddress(generateRandomString(20))
+		// .withSecondaryPhone(app.getContactHelper().phoneRandomizer());
+		//
+		//
+		//
+		// list.add(new Object[]{contact});
+		// }
 		return list.iterator();
 	}
-	
-	@DataProvider
-	public Iterator<Object[]> randomValidContactGenerator(){
-		List<Object[]> list = new ArrayList<Object[]>();
-		for(int i =0;i<1;i++){
-			ContactData contact = new ContactData()
-			.withName(generateRandomString(10))
-			.withSecondName(generateRandomString(10))
-			.withPostAddress(generateRandomString(20))
-			.withHomePhoneNum(app.getContactHelper().phoneRandomizer())
-			.withMobilePhonNum(app.getContactHelper().phoneRandomizer())
-			.withWorkPhoneNum(app.getContactHelper().phoneRandomizer())
-			.withPostPrimary(app.getContactHelper().emailRandomizer())
-			.withPostSecondary(app.getContactHelper().emailRandomizer())
-			.withDay("18")
-			.withMonth("January")
-			.withYear("1992")
-			.withSecondaryPostAddress(generateRandomString(20))
-			.withSecondaryPhone(app.getContactHelper().phoneRandomizer());
 
-			
-			
-			list.add(new Object[]{contact});
-		}
-		return list.iterator();
-	}
-	
-	
-	public String generateRandomString(int index)
-	{
-		Random rnd = new Random();
-		if (rnd.nextInt(5)==0)
-		{
-			return "";
-		}
-		else{
-		return app.getGroupHelper().CharacterRandomizer(index);
-		}
-		
-	}
+	// public String generateRandomString(int index)
+	// {
+	// Random rnd = new Random();
+	// if (rnd.nextInt(5)==0)
+	// {
+	// return "";
+	// }
+	// else{
+	// return app.getGroupHelper().CharacterRandomizer(index);
+	// }
+	//
+	// }
 }
