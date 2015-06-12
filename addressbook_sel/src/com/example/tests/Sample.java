b7950c09715336f7a9a6330b41e3076aa2d32e92
kyroikoneko <kotenok.nataly@gmail.com>
2015-06-13 00:58:44 +0400

    8.1.2
    exeption
    java.sql.SQLException: No suitable driver found for
    jdbs:mysql://localhost/addressbook?login=root&password=
    at java.sql.DriverManager.getConnection(Unknown Source)
    at java.sql.DriverManager.getConnection(Unknown Source)
    at com.example.fw.JdbcHelper.<init>(JdbcHelper.java:24)
    at com.example.tests.Sample.main(Sample.java:17)
    Exception in thread "main" java.lang.NullPointerException
    at com.example.fw.JdbcHelper.listGroups(JdbcHelper.java:47)
    at com.example.tests.Sample.main(Sample.java:18)

--- addressbook_sel/src/com/example/tests/Sample.java
+++ addressbook_sel/src/com/example/tests/Sample.java
@@ -1,7 +1,22 @@
 package com.example.tests;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+import com.example.fw.ApplicationManager;
+import com.example.fw.JdbcHelper;
 
 public class Sample {
-	public static void main(String[] args) {
+	public static void main (String[] args) throws FileNotFoundException, IOException{
+		
+		Properties properties = new Properties();
+	    properties.load(new FileReader (new File( "application.properties")) );
+	    ApplicationManager app=new ApplicationManager(properties);
+	    JdbcHelper jdbs = new JdbcHelper(app, "jdbs:mysql://localhost/addressbook?user=root&password=");
+	    System.out.println(jdbs.listGroups());
+	
 
 		// String b = "+7(816)615-0899";
 		// b = b.trim();
